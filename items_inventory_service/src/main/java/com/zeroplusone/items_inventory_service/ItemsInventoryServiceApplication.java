c25a35fe3db5edabf90532a8b7105dbd3953f2f7
Abhishek <abhishek@github.com>
2024-12-30 18:08:27 +0530

    cosmetic changes

---
+++ items_inventory_service/src/main/java/com/zeroplusone/items_inventory_service/ItemsInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zeroplusone.items_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ items_inventory_service/src/test/java/com/zeroplusone/items_inventory_service/ItemsInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroplusone.items_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
